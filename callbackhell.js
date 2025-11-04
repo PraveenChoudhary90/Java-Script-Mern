a30e9e84af23ff6fe1c6d5abf72fa2d207cf7366
Praveen Choudhary <pc852180@gmail.com>
2025-11-04 20:02:19 +0530

    Callback

---
+++ callbackhell.js
@@ -0,0 +1,14 @@
+
+
+setTimeout(()=>{
+    console.log("A");
+    setTimeout(()=>{
+        console.log("B");
+        setTimeout(()=>{
+            console.log("C");
+            setTimeout(()=>{
+                console.log("D")
+            },1000)
+        },1000)
+    },1000)
+},1000)
