785c776b39d8ebdf2239baba9c4b25a1c1322124
Praveen Choudhary <pc852180@gmail.com>
2025-11-04 19:56:25 +0530

    Callback

--- Callback.js
+++ Callback.js
@@ -1,13 +1,55 @@
 
-function mygreet(name, callback){
-    console.log("Hello", name);
-    callback();
+// function mygreet(name, callback){
+//     console.log("Hello", name);
+//     callback();
     
+// }
+
+
+// function SayHello(){
+//     console.log("Good bye")
+// }
+
+// mygreet("Praveen choudhary Jii",SayHello);
+
+
+// another example
+
+function Mydata(callback){
+    setTimeout(() => {
+        console.log("Fateched data succesfully");
+        callback();
+    }, 1000);
 }
 
+Mydata(()=>{
+    console.log("Now data is comming")
+});
 
-function SayHello(){
-    console.log("Good bye")
+
+function fetchData(callback) {
+  setTimeout(() => {
+    console.log("Data fetched!");
+    callback();
+  }, 1000);
 }
 
-mygreet("Praveen choudhary Jii",SayHello);
+fetchData(() => {
+  console.log("Now processing data...");
+});
+
+
+
+function Showdata(callback){
+    setTimeout(()=>{
+        console.log("FAteched");
+        callback();
+    },2000)
+
+}
+
+Showdata(()=>{
+    console.log("now see")
+})
+
+
