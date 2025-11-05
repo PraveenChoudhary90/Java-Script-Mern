833b6d99a8fb403dd6e3a2dd5ef353bf73806eb1
Praveen Choudhary <pc852180@gmail.com>
2025-11-05 18:54:20 +0530

    event loop

---
+++ EventLoop1.js
@@ -0,0 +1,26 @@
+
+// console.log("a")
+// setImmediate(()=>{
+//     console.log("Async")
+// },2000)
+// console.log("b")
+// console.log("c")
+// console.log("d")
+// console.log("e")
+
+
+console.log("TAsk1");
+console.log("Task2");
+setImmediate(()=>{
+    console.log("TAsk3");
+},3000)
+console.log("Task4");
+
+
+// console.log("data1");
+// console.log("data2");
+// setInterval(()=>{
+//     console.log("Data3")
+// },3000)
+// console.log("data4");
+// console.log("data5");
